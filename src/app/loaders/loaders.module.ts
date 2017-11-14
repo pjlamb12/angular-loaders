65e9038a60eca9d80e062750705cafcae8575c89
Preston Lamb <preston.j.lamb@gmail.com>
2017-11-13 23:37:28 -0700

    move pulsing circles to its own component

--- src/app/loaders/loaders.module.ts
+++ src/app/loaders/loaders.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { RotatingSquareComponent } from './rotating-square/rotating-square.component';
+import { PulsingCirclesComponent } from './pulsing-circles/pulsing-circles.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoaderComponent, RotatingSquareComponent],
+  declarations: [LoaderComponent, RotatingSquareComponent, PulsingCirclesComponent],
   exports: [LoaderComponent]
 })
 export class LoadersModule { }
---
+++ src/app/loaders/pulsing-circles/pulsing-circles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'ng-pulsing-circles',
+  templateUrl: './pulsing-circles.component.html',
+  styleUrls: ['./pulsing-circles.component.scss']
+})
+export class PulsingCirclesComponent implements OnInit {
+	@Input() spinnerColor: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
