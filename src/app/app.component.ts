e803f628d7cba148ef169ae615d243a9ea8c6522
Preston Lamb <preston.j.lamb@gmail.com>
2018-07-14 00:15:54 -0600

    fix the demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	public spinnerType: string = 'rotating-square';
+
+	setSpinnerType(type: any) {
+		this.spinnerType = type;
+	}
 }
