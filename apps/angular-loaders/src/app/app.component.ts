e4d29172013f9e9f22c7082eebd7f869fac241bc
Preston Lamb <preston.j.lamb@gmail.com>
2023-06-15 15:28:05 -0600

    feat: get the demo form initial work done

--- apps/angular-loaders/src/app/app.component.ts
+++ apps/angular-loaders/src/app/app.component.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
-import { LoaderComponent } from 'ng-loaders';
+import { LoaderComponent, SpinnerType } from 'ng-loaders';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Observable, map, startWith, tap } from 'rxjs';
+import { AsyncPipe, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
 	standalone: true,
-	imports: [NxWelcomeComponent, LoaderComponent],
+	imports: [
+		NxWelcomeComponent,
+		LoaderComponent,
+		NgIf,
+		AsyncPipe,
+		ReactiveFormsModule,
+		NgFor,
+		UpperCasePipe,
+	],
 	selector: 'angular-loaders-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	title = 'angular-loaders';
+	spinnerTypeOptions: SpinnerType[] = [
+		'cube-grid',
+		'ellipsis',
+		'growing-bars',
+		'growing-circle',
+		'pulsing-circles',
+		'rotating-cubes',
+		'rotating-dots',
+		'rotating-square',
+	];
+	colorOptions = ['red', 'blue', 'yellow'];
+	public spinnerDemoForm = this._fb.group({
+		spinnerType: ['cube-grid'],
+		spinnerColor: ['red'],
+	});
+
+	public formValues$: Observable<{
+		spinnerColor: string;
+		spinnerType: SpinnerType;
+	}> = this.spinnerDemoForm.valueChanges.pipe(
+		startWith(this.spinnerDemoForm.value),
+		map(({ spinnerColor, spinnerType }) => {
+			return {
+				spinnerColor: spinnerColor || 'red',
+				spinnerType: (spinnerType || 'cube-grid') as SpinnerType,
+			};
+		}),
+		tap(({ spinnerColor }) => {
+			// set the css custom property of spinnerColor
+			document.documentElement.style.setProperty(
+				'--spinnerColor',
+				spinnerColor as string
+			);
+		})
+	);
+
+	constructor(private _fb: FormBuilder) {}
 }
